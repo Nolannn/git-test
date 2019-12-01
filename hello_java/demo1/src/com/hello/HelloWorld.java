45e3ba96e48ce2cf4ac39026109c5faa60def748
ZhichaoYan <329225749@qq.com>
2019-12-02 01:06:24 +0800

    commit

---
+++ hello_java/demo1/src/com/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+    }
+    public void Test(){
+        System.out.println("test");
+    }
+}
